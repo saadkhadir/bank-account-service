8c2e1cbfe5b99d8f0d9c3e56add6829ce96043d3
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-10-06 21:42:50 +0100

    data-rest commit

--- src/main/java/org/example/bankaccountservice/repositories/BankAccountRepository.java
+++ src/main/java/org/example/bankaccountservice/repositories/BankAccountRepository.java
@@ -1,11 +1,13 @@
 package org.example.bankaccountservice.repositories;
 
 import org.example.bankaccountservice.entities.BankAccount;
+import org.example.bankaccountservice.enums.AccoutType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-    List<BankAccount> id(String id);
+    List<BankAccount> findByType(AccoutType type);
 }
--- src/main/java/org/example/bankaccountservice/web/AccountRestController.java
+++ src/main/java/org/example/bankaccountservice/web/AccountRestController.java
@@ -3,13 +3,14 @@ package org.example.bankaccountservice.web;
 import org.example.bankaccountservice.entities.BankAccount;
 import org.example.bankaccountservice.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class AccountRestController {
     @Autowired
     private BankAccountRepository bankAccountRepository;
@@ -24,6 +25,23 @@ public class AccountRestController {
     public BankAccount bankAccount(@PathVariable String id) {
         return bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
     }
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount) {
+        if (bankAccount.getId()==null) bankAccount.setId(UUID.randomUUID().toString());
+        return bankAccountRepository.save(bankAccount);
+    }
 
-
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id, @RequestBody BankAccount bankAccount) {
+        BankAccount account=bankAccountRepository.findById(id).orElseThrow();
+        if(bankAccount.getBalance()!=null) account.setBalance(bankAccount.getBalance());
+        if(bankAccount.getCurrency()!=null)account.setCurrency(bankAccount.getCurrency());
+        if(bankAccount.getType()!=null)account.setType(bankAccount.getType());
+        if(bankAccount.getCreatedAt()!=null)account.setCreatedAt(new Date());
+        return bankAccountRepository.save(account);
+    }
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount(@PathVariable String id) {
+         bankAccountRepository.deleteById(id);
+    }
 }
